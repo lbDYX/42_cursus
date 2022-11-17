79bbba71642470b2dbc62a1f88d9b2252126a6ca
Dannyel Cortes Romero <dacortes@cbr3s2.42barcelona.com>
2022-11-17 15:45:55 +0100

    entrega1

--- get_next_line/main_bonus.c
+++ get_next_line/main_bonus.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   main_bonus.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: dacortes <dacortes@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/11/16 10:56:12 by dacortes          #+#    #+#             */
-/*   Updated: 2022/11/17 14:58:44 by dacortes         ###   ########.fr       */
+/*   Created: 2022/11/17 15:00:48 by dacortes          #+#    #+#             */
+/*   Updated: 2022/11/17 15:27:28 by dacortes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@
 #define RED "\033[31m"
 #define GREEN "\033[32m"
 #define YELLOW "\033[33m"
+#define BLUE "\033[34m"
 
 #define ligth "\033[1m"
 #define dark = "\033[2m"
@@ -25,8 +26,9 @@
 int	main(void)
 {
 	int	fd = open("arch_txt/multiple_nl.txt", O_RDONLY);
+	int fd1 = open("arch_txt/multiple_nl.txt", O_RDONLY);
 	char	*line;
-	int i = 0;
+	int i = 1;
 
 	line = NULL;
 	if (fd < 0)
@@ -34,13 +36,25 @@ int	main(void)
 		printf(RED"Error!!"YELLOW"-->memory allocation\n"END);
 		return (0);
 	}
+	printf(BLUE"FD [giant_line.txt]\n"END);
+	while (i < BUFFER_SIZE)
+	{
+		line = get_next_line(fd1);
+		printf("line ["YELLOW"%d"END"]"GREEN" content :"END"%s", i++, line);
+		printf(YELLOW"\n-->"END"numbers bytes ["GREEN ligth"%lu"END"]\n",ft_strlen(line));
+		if (line == NULL)
+			break ;
+	}
+	i = 1;
+	printf(BLUE"\nFD [multiple_nl.txt]\n"END);
 	while (i < BUFFER_SIZE)
 	{
 		line = get_next_line(fd);
 		printf("line ["YELLOW"%d"END"]"GREEN" content :"END"%s", i++, line);
-		printf(YELLOW"-->"END"numbers bytes ["GREEN ligth"%lu"END"]\n",ft_strlen(line));
-		if (line== NULL)
+		printf(YELLOW"\n-->"END"numbers bytes ["GREEN ligth"%lu"END"]\n",ft_strlen(line));
+		if (line == NULL)
 			break ;
 	}
+	free(line);
 	return (0);
 }
