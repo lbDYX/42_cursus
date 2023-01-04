c0fa83ee84adb46bbe420c870a88a669ceab4b40
dacortes <87203430+lbDYX@users.noreply.github.com>
2023-01-04 12:26:29 +0100

    Update ft_isascii.c

--- libft/ft_isascii.c
+++ libft/ft_isascii.c
@@ -9,9 +9,6 @@
 /*   Updated: 2022/10/10 18:08:33 by dacortes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-/*Verifica si está dentro del rango de 0 a 127 en valor a retornar será 1 si
-esta dentro del rango de lo contrario retornara 0*/
-
 #include"libft.h"
 
 int	ft_isascii(int c)
