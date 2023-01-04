5e0d7e60d68de103b0a62961e48c7849318494b4
dacortes <87203430+lbDYX@users.noreply.github.com>
2023-01-04 12:35:11 +0100

    Update ft_strrchr.c

--- libft/ft_strrchr.c
+++ libft/ft_strrchr.c
@@ -9,11 +9,6 @@
 /*   Updated: 2022/10/13 10:59:35 by dacortes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-/*La función es idéntica al ft_strrchr la diferencia es que lo caliza la ultima 
-ocurrencia de c, es decir empieza a iterar el str al contrario, verifica si el 
-str es igual a 0 retornando NULL, si str igual a NULL retorna str y si no 
-encontró nada retorna NULL;*/
-
 #include"libft.h"
 
 char	*ft_strrchr(const char *s, int c)
