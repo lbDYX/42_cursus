10ab11ded4d64cba9636d0fbe6c2d75f6f7f5272
dacortes <87203430+lbDYX@users.noreply.github.com>
2023-01-04 12:26:56 +0100

    Update ft_isdigit.c

--- libft/ft_isdigit.c
+++ libft/ft_isdigit.c
@@ -9,9 +9,6 @@
 /*   Updated: 2022/10/10 17:05:28 by dacortes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-/*La función verifica si es un número retornara 1 de lo contrario retornara 0,
-toma el valor de la tabla ascii en entero ejemplo '0' = 48.*/
-
 #include"libft.h"
 
 int	ft_isdigit(int c)
