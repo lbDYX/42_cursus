ac71c3d1c778e08e4f26a4cb44ddb417fa564d66
dacortes <87203430+lbDYX@users.noreply.github.com>
2023-01-04 12:30:20 +0100

    Update ft_memmove.c

--- libft/ft_memmove.c
+++ libft/ft_memmove.c
@@ -9,12 +9,6 @@
 /*   Updated: 2022/10/11 10:02:38 by dacortes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-/*Realiza una copia segura de src a dst para evitar solapamiento.
-Se verifica si dts y src tienen un valor diferente a 0 o nulll, se pregunta si 
-la resta de los tamaños tanto en src como en dst son menores al len solicitado 
-y evita que se realice el solapamiento al intentar copiar, de lo contrario 
-realiza la copia con normalidad*/
-
 #include"libft.h"
 
 void	*ft_memmove(void *dst, const void *src, size_t len)
