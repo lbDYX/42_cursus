64a8882c0ead38d7b5405afab3d3d2257d2463bf
dacortes <87203430+lbDYX@users.noreply.github.com>
2023-01-04 12:26:06 +0100

    Update ft_isalpha.c

--- libft/ft_isalpha.c
+++ libft/ft_isalpha.c
@@ -10,10 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 #include"libft.h"
-/*La función verifica si es una letra minúsculas o mayúsculas,
-toma el valor de la tabla ascii en entero ejemplo a = 97 se
-puede ingresar tanto 'a' como 97 y validara si está en el rango.
-retorna 1 si es una letra*/
+
 int	ft_isalpha(int c)
 {
 	if (c >= 'a' && c <= 'z')
