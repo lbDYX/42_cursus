5c2806210b4070291ee39788ba1c13aa01155fd3
dacortes <87203430+lbDYX@users.noreply.github.com>
2023-01-04 12:33:35 +0100

    Update ft_strlcpy.c

--- libft/ft_strlcpy.c
+++ libft/ft_strlcpy.c
@@ -9,13 +9,6 @@
 /*   Updated: 2022/10/11 10:30:03 by dacortes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-/*Toma el largo del dst y garantiza la terminación de null, después realiza 
-la copia.
-Se verifica el tamaño del src y se pregunta si el dstsize == 0 si es igual 
-retorna el len de lo que intento copiar, de lo contrario realiza la copia y se 
-garantiza que termine en null al finalizar la copia, retorna el len de lo que 
-se copió.*/
-
 #include"libft.h"
 
 size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
